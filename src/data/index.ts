5548f88dceca3ed5c788c48770f2d51416af5cb5
personal <shaneel_kumar@live.com>
2022-12-20 15:16:30 +1300

    add misc projects section

--- src/data/index.ts
+++ src/data/index.ts
@@ -13,7 +13,6 @@ export const projects: Project[] = [
     description: 'Marketplace for neighbours to barter and give away food, building stronger relationships with those around you.',
     githubLink: 'https://github.com/Vyonyx/neighbourly',
     liveLink: 'https://github.com/Vyonyx/neighbourly',
-
   },
   {
     name: 'At What Cost',
@@ -30,9 +29,25 @@ export const projects: Project[] = [
     githubLink: 'https://github.com/Vyonyx/neighbourly',
     liveLink: 'https://github.com/Vyonyx/neighbourly',
   },
-  // {
-  //   name: 'At What Cost',
-  //   thumbnail: 'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-  //   description: 'Budget tracking app used to understand what you\'re spending money on so you can make informed decisions about your finances.'
-  // },
+]
+
+export const miscProjects: Project[] = [
+  {
+    name: 'CV Builder',
+    thumbnail: 'https://images.unsplash.com/photo-1519222970733-f546218fa6d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    description: 'Marketplace for neighbours to barter and give away food, building stronger relationships with those around you.',
+    githubLink: 'https://github.com/Vyonyx/cv-builder',
+  },
+  {
+    name: 'Minesweeper',
+    thumbnail: 'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    description: 'Budget tracking app used to understand what you\'re spending money on so you can make informed decisions about your finances.',
+    githubLink: 'https://github.com/Vyonyx/neighbourly',
+  },
+  {
+    name: 'CV Builder',
+    thumbnail: 'https://images.unsplash.com/photo-1519222970733-f546218fa6d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    description: 'Marketplace for neighbours to barter and give away food, building stronger relationships with those around you.',
+    githubLink: 'https://github.com/Vyonyx/cv-builder',
+  },
 ]
