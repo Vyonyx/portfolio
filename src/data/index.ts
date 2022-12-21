7e17e3946b9f3aefb6b6e00d14dc3ef09eb9ac7e
personal <shaneel_kumar@live.com>
2022-12-21 16:14:13 +1300

    create list item component and shift experience info into data file for dynamic display

--- src/data/index.ts
+++ src/data/index.ts
@@ -57,3 +57,31 @@ export const architectureProject: Project =  {
   thumbnail: '/swarm-hero.jpg',
   description: 'My architectural thesis on the use of simulation to study demographic interactions and create a built environment using the visual data.'
 }
+
+export interface Experience {
+  title: string;
+  company: string;
+  year: string;
+  description: string;
+}
+
+export const experience: Experience[] = [
+  {
+    title: 'Full Stack Web Developer',
+    company: 'Dev Academy Aotearoa',
+    year: 'Aug - Dec 2022',
+    description: `Graduated from an intense 15 week web development bootcamp that mimics what it’s like to work in development teams. The programme heavily focused on learning a modern web dev stack and how we can work in teams to deliver products through the agile methodology. There was also emphasis on learning soft skills that help us navigate working in a variety of teams.`
+  },
+  {
+    title: 'Architectural Graduate (Intermediate)',
+    company: 'Prime Designs',
+    year: 'Sep 2020 - Aug 2022',
+    description: `I took projects from client consultation to building consent submission, working on projects that fell into a wide spectrum of difficulty. The role involved leading my own projects by coordinating with external consultants/engineers to collate and deliver a set of construction documents. I also managed client expectations, deadlines and answered builder’s queries during construction phase.`
+  },
+  {
+    title: 'Architectural Graduate (Team Lead)',
+    company: 'Design Network Architecture',
+    year: 'Jan 2019 - Mar 2020',
+    description: `I was responsible for project managing a team of 3 junior developers while they learned the workflow/process and also provided mentorship and upskilling sessions on an individual basis. This was my first role in a leadership position where I found a natural ability to coordinate a team towards an outcome and how to work with different types of people. This position was also a 2nd promotion from starting as a junior, to intermediate, and then finally team lead. I was told that my quick progression over the year was due to my technical proficiency and the delivery of high quality work in an efficient manner.`
+  },
+]
