f8a822bae79d7cb4da8e2e196b357f35f7ac07af
personal <shaneel_kumar@live.com>
2023-01-22 00:09:10 +1300

    add 0 opacity to about me section to fix stutter on page load

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,50 +1,61 @@
-import gsap from 'gsap'
-  import { ScrollTrigger } from 'gsap/ScrollTrigger'
-  gsap.registerPlugin(ScrollTrigger)
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
-export function renderAnimation(section:string) {
-  const title = document.getElementById(section)
-  const cards = document.querySelectorAll(`[data-card='${section}']`)
+export function renderAnimation(section: string) {
+  const title = document.getElementById(section);
+  const cards = document.querySelectorAll(`[data-card='${section}']`);
 
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: cards,
-      start: 'top 80%',
-      end: 'top 30%',
-      scrub: 2
+      start: "top 80%",
+      end: "top 30%",
+      scrub: 2,
     },
-  })
+  });
 
   tl.from(title, {
     opacity: 0,
     duration: 1,
     y: window.innerWidth < 768 ? -10 : -50,
-    ease: 'sin.easeInOut'
-  })
-  tl.from(cards, {
-    opacity: 0,
-    duration: 1,
-    y: 100,
-    ease: 'sin.easeInOut'
-  }, '<')
+    ease: "sin.easeInOut",
+  });
+  tl.from(
+    cards,
+    {
+      opacity: 0,
+      duration: 1,
+      y: 100,
+      ease: "sin.easeInOut",
+    },
+    "<"
+  );
 }
 
-export function basicRevealAnimation(section:Element) {
+export function basicRevealAnimation(section: Element) {
   const tl = gsap.timeline({
     defaults: {
-      ease: 'sin.easeInOut'
+      ease: "sin.easeInOut",
     },
-  })
+  });
 
-  tl.from(section, {
-    opacity: 0,
-    y: 100,
-    duration: 2,
-  })
+  tl.fromTo(
+    section,
+    {
+      opacity: 0,
+      y: 100,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 2,
+    }
+  );
 }
 
-export function scrollRevealAnimation(section:Element) {
-  return (scrollerEnd:number = 30, scrollerStart:number = 80) => {
+export function scrollRevealAnimation(section: Element) {
+  return (scrollerEnd: number = 30, scrollerStart: number = 80) => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: section,
@@ -53,13 +64,13 @@ export function scrollRevealAnimation(section:Element) {
         scrub: 2,
       },
       defaults: {
-        ease: 'sin.easeInOut'
+        ease: "sin.easeInOut",
       },
-    })
-  
+    });
+
     tl.from(section, {
       opacity: 0,
       y: 100,
-    })
-  }
+    });
+  };
 }
